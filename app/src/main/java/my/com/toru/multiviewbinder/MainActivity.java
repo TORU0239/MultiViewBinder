604db31bed02aa3610ae2601698f4736ee1d3487
Toru Choi <toru.choi@11street.my>
2018-05-04 11:28:17 +0800

    Examples for applying multiadapter of RecyclerView

---
+++ app/src/main/java/my/com/toru/multiviewbinder/MainActivity.java
@@ -0,0 +1,13 @@
+package my.com.toru.multiviewbinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
