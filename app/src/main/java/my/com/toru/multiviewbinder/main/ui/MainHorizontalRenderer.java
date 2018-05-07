ab36d8f9b72231b21e4eeda07be24b9b7a933d2f
Toru Choi <toru.choi@11street.my>
2018-05-07 15:59:09 +0800

    horizontal listview

--- app/src/main/java/my/com/toru/multiviewbinder/main/ui/MainHorizontalRenderer.java
+++ app/src/main/java/my/com/toru/multiviewbinder/main/ui/MainHorizontalRenderer.java
@@ -22,6 +22,6 @@ public class MainHorizontalRenderer extends AbsViewRenderer<MainHorizontalData, 
 
     @Override
     public void bindView(MainHorizontalData model, MainHorizontalViewHolder viewHolder) {
-        viewHolder.bindItem();
+        viewHolder.bindItem(model.getHorizontalDetailDataList());
     }
 }
--- app/src/main/java/my/com/toru/multiviewbinder/main/ui/MainHorizontalViewHolder.java
+++ app/src/main/java/my/com/toru/multiviewbinder/main/ui/MainHorizontalViewHolder.java
@@ -1,5 +1,6 @@
 package my.com.toru.multiviewbinder.main.ui;
 
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
@@ -18,6 +19,7 @@ public class MainHorizontalViewHolder extends RecyclerView.ViewHolder {
         recyclerView = itemView.findViewById(R.id.horizontal_recyclerview);
         adapter = new MainHorizontalDetailAdapter(itemView.getContext());
         recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(itemView.getContext(), LinearLayoutManager.HORIZONTAL, false));
     }
 
     protected void bindItem(List<MainHorizontalDataDetail> horizontalDataDetailList){
